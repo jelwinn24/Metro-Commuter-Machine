fb5792f75b4348204f3af65084360ea028517a6e
Jelwinn24 <jelwinn@gmail.com>
2016-07-05 21:24:33 +1000

    Uploaded Background flattened Image

--- MetroApp/MetroApp/Form1.Designer.vb
+++ MetroApp/MetroApp/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmTitle
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,22 +20,25 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmTitle))
         Me.SuspendLayout()
         '
-        'Form1
+        'FrmTitle
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(755, 454)
         Me.ControlBox = False
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "FrmTitle"
+        Me.Text = "Metro Commuter Machine"
         Me.ResumeLayout(False)
 
     End Sub
-
 End Class
--- MetroApp/MetroApp/My Project/Application.Designer.vb
+++ MetroApp/MetroApp/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MetroApp.Form1
+            Me.MainForm = Global.MetroApp.FrmTitle
         End Sub
     End Class
 End Namespace
